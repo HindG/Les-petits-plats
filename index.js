9c0e63d28bb7d9e16ae8fabafa463f8f64ead756
Hind <guessoushind@gmail.com>
2022-03-10 10:29:55 +0100

    :sparkles: Added ingredients list

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+// Déclaration des variables
+const ingredientsTab = getIngredients(recipes);
+let ingredientsTabFiltered = [...ingredientsTab]; // clone d'un tableau avec le "spread operator"
+const ingredientsBtn = document.getElementById('ingredients');
+const devicesBtn = document.getElementById('devices');
+const ingredientsListContainer = document.getElementById('ingredients-list-container');
+const ingredientsChevronDown = document.getElementById('ingredients-chevron-down');
+const ingredientsChevronUp = document.getElementById('ingredients-chevron-up');
+const ingredientsList = document.getElementById('ingredients-list');
+const tagContainer = document.getElementById('tags-container');
+let ingredientsInput = document.getElementById('input-ingredients');
+
+// Ecouter les évènements 
+ingredientsChevronDown.addEventListener('click', displayIngredients(ingredientsTab));
+ingredientsChevronUp.addEventListener('click', hideIngredients);
+ingredientsInput.addEventListener('change', () => {
+    let newIngredients = filterIngredients(ingredientsTab, ingredientsInput.value.toLowerCase())
+    displayIngredients(newIngredients)
+})
+
 // Afficher les recettes
 function getRecipe() {
     const container = document.getElementById('container');
@@ -8,7 +28,7 @@ function getRecipe() {
         imageDiv.classList.add('img-card');
         article.appendChild(imageDiv);
         const header = document.createElement('header');
-        header.classList.add('padded-container','d-flex');
+        header.classList.add('padded-container','d-flex','title-container');
         article.appendChild(header);
         const h3 = document.createElement('h3');
         h3.classList.add('lato');
@@ -63,16 +83,61 @@ function getRecipe() {
 getRecipe();
 
 // Algorithme qui récupère tous les ingrédients
-const ingredientsTab = getIngredients(recipes);
-
 function getIngredients(recipes) {
     let tab = [];
     for (let i = 0; i < recipes.length; i++) {
         for (let j = 0; j < recipes[i].ingredients.length; j++) {
-            //vérifier que l'ingrédient n'est pas déjà là
-            tab.push(recipes[i].ingredients[j].ingredient);
+            let ingredientLowerCase = recipes[i].ingredients[j].ingredient.toLowerCase();
+            if (tab.indexOf(ingredientLowerCase) === -1 ) {
+                tab.push(ingredientLowerCase);
+            }
         }
     }
-    console.log(tab);
     return tab;
+}
+
+/**
+ * Affiche les ingrédients
+ * @param {array} ingredients Tableau d'ingredient
+ */
+function displayIngredients(ingredients) {
+    // 1 -> vider la liste
+    ingredientsList.innerHTML = "";
+    // 2 -> remplir la liste
+    ingredientsBtn.style.display = "none";
+    ingredientsListContainer.style.display = "block";
+    ingredients.forEach((element) => {
+        const spanIngredient = document.createElement('span');
+        spanIngredient.classList.add('list-element','lato');
+        spanIngredient.innerText = element[0].toUpperCase() + element.slice(1);
+        spanIngredient.addEventListener('click', filterRecipes);
+        ingredientsList.appendChild(spanIngredient);
+    })  
+}
+
+/**
+ * Filtre le tableau des ingredients
+ * @param {array} fullIngredientsTab le tableau complet des ingredients
+ * @param {string} searchInput la valeur à rechercher dans les ingrédients
+ * @returns {array} un nouveau tableau d'ingrédient filtré
+ */
+function filterIngredients(fullIngredientsTab, searchInput) {
+    let newIngredientsTab = [];
+    for (const ingredient of fullIngredientsTab) {
+        if (ingredient.indexOf(searchInput) > -1) {
+            newIngredientsTab.push(ingredient)
+        }
+    }
+    return newIngredientsTab;
+}
+// test 1 : filterIngredients(['lait de coco', 'coca cola'], 'coco') -> ['lait de coco']
+
+function main() {
+        
+}
+
+// Replier liste ingrédients
+function hideIngredients() {
+    ingredientsBtn.style.display = "block";
+    ingredientsListContainer.style.display = "none";
 }
